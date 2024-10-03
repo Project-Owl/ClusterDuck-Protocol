ee27072bb0c47d6299a5caf7c2a38fd7c468d0e8
faradaym <rcheyenne.truss@gmail.com>
2024-10-02 22:25:28 -0500

    handle incoming achat packets
    Signed-off-by: faradaym <rcheyenne.truss@gmail.com>

--- src/Ducks/MamaDuck.cpp
+++ src/Ducks/MamaDuck.cpp
@@ -107,6 +107,23 @@ void MamaDuck::handleReceivedPacket() {
             loginfo_ln("handleReceivedPacket: packet RELAY DONE");
           }
         break;
+        case topics::achat:{
+          packet.timeReceived = millis();
+          duckNet->addToAtakBuffer(packet);
+
+          std::vector<byte> data;
+          byte numPairs = 1;
+          data.insert(data.end(), numPairs);
+          data.insert(data.end(), packet.muid.begin(), packet.muid.end());
+
+          err = duckRadio.relayPacket(rxPacket);
+          if (err != DUCK_ERR_NONE) {
+            logerr_ln("====> ERROR handleReceivedPacket failed to relay. rc = %d",err);
+          } else {
+            loginfo_ln("handleReceivedPacket: packet RELAY DONE");
+          }
+        }
+        break;
         default:
           err = duckRadio.relayPacket(rxPacket);
           if (err != DUCK_ERR_NONE) {
